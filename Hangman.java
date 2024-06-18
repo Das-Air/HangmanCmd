77e9fde5ac62d1981dad785b1b8c20fdbf910930
DasAerus <aircraft710@gmail.com>
2024-06-18 21:59:48 +0200

    hotfix missing catch case

--- Hangman.java
+++ Hangman.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  * @author DasAerus 
  * A Class responsible for handling game Events and sending them to {@link KonsolenAnzeige} to be shown to the user in the Console
@@ -17,6 +23,27 @@ public class Hangman {
 
     public static void main(String[] args) {
 
+    }
+
+    /**
+     * @param path
+     */
+    public void neuesWort(String path){
+        try{
+        File file = new File(path); 
+        final RandomAccessFile rndFile = new RandomAccessFile(file, "r");
+        final long randomLocation = (long) (Math.random() * rndFile.length());
+        rndFile.seek(randomLocation);
+        rndFile.readLine(); //will move file pointer to the next line, as it might be in the middle of one due to seek()
+        begriff = rndFile.readLine();
+        rndFile.close(); 
+        }
+        catch(FileNotFoundException error){
+            System.out.printf("Keine Valide Datei/Dateipfad unter %s!", path);
+        }
+        catch(IOException error){
+            System.out.printf("Fehler während dem Zugriff auf die Datei unter %s!", path);
+        }
     }
 
     //ToDo logic
